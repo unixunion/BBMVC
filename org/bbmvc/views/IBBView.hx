3afe10433727947cda75f3167f4229b08ee75c3b
Kegan Holtzhausen <kegan.holtzhausen@unibet.com>
2014-01-17 14:03:44 +0100

    accessor wrong for typical Sprite extends

--- org/bbmvc/views/IBBView.hx
+++ org/bbmvc/views/IBBView.hx
@@ -2,7 +2,7 @@ package org.bbmvc.views;
 
 interface IBBView 
 {
-	var name:String;
+	var name(get,set):String;
 	var viewId:String;
 	function destroy():Void;
 	function update(?updateType:String="", ?data:Dynamic=null):Void;
